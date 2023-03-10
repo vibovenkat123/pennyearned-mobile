58701b83a5f04a1cd2fd1f93f9f32f4a0e70bd52
Vaibhav Venkat <vibovenkat@gmail.com>
2023-03-09 18:05:21 -0600

    Initial Commit

---
+++ pennyearned/pennyearnedApp.swift
@@ -0,0 +1,17 @@
+//
+//  pennyearnedApp.swift
+//  pennyearned
+//
+//  Created by Vaibhav Venkat on 3/9/23.
+//
+
+import SwiftUI
+
+@main
+struct pennyearnedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
