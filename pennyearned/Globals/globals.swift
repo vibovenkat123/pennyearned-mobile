8dcbf1b74c65cd9204398b02d5fa7ffa1b8114fe
Vaibhav Venkat <vibovenkat@gmail.com>
2023-04-20 20:28:07 -0500

    fix: change to prod url

--- pennyearned/Globals/globals.swift
+++ pennyearned/Globals/globals.swift
@@ -12,12 +12,12 @@ struct Globals {
     @Environment(\.colorScheme) var colorScheme
 //    static let userServer: String = "https://aq9q963cc2.execute-api.us-east-1.amazonaws.com/dev"
 //    static let expensesServer: String = "https://aq9q963cc2.execute-api.us-east-1.amazonaws.com/dev"
-    static let userServer: String = "http://192.168.1.181:3002"
-    static let expensesServer: String = "http://192.168.1.181:3001"
+    static let userServer: String = "https://aq9q963cc2.execute-api.us-east-1.amazonaws.com/dev"
+    static let expensesServer: String = "https://aq9q963cc2.execute-api.us-east-1.amazonaws.com/dev"
     static let keychain = Keychain(service: "com.vaibhav.pennyearned.keychain")
                             .label("pennyearned credentials")
                             .synchronizable(true)
-    static var user = User(id: "", email: "", password: "", username: "", date_created: "", date_updated: "", budget: 0)
+    static var user = User(id: "", email: "", password: "", username: "", date_created: "", date_updated: "")
 }
 struct User:Codable {
     var id: String
@@ -26,7 +26,6 @@ struct User:Codable {
     var username: String
     var date_created: String
     var date_updated: String
-    var budget: Int
 }
 
 struct Expense: Codable{
